cb4ac37f4614f0ff01cfaf43ccf416c19bf144c9
sivakatakam <119718935+sivakatakam@users.noreply.github.com>
2023-06-15 21:46:48 +0530

    import java.util.*; public class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String c=sc.nextLine();         System.out.println(c);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String c=sc.nextLine();
+        System.out.println(c);
+    }
+}
